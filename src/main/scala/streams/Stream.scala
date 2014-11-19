352d3f75a2723695201921efc0b39adc626db87d
Aggelos Biboudis <biboudis@gmail.com>
2014-11-19 19:27:33 +0200

    Add array builder instead of buffer and specialization.

--- src/main/scala/streams/Stream.scala
+++ src/main/scala/streams/Stream.scala
@@ -1,18 +1,43 @@
 package streams
 
 import scala.reflect.ClassTag
+import scala.collection.mutable.ArrayBuilder
 import scala.Array
 import scala.collection.mutable.ArrayBuffer
 
-final class Stream[T: ClassTag](val streamf: (T => Boolean) => Unit) {
 
-  def toArray(): Array[T] = 
-    foldLeft(new ArrayBuffer[T])((a: ArrayBuffer[T], value: T) => a += value).toArray
+trait SpecializedArrayBuilder[@specialized(Long) A] {
+  def +=(a: A)
+  def result: Array[A]
+}
+
+trait LowPrioritySpecializedArrayBuilder {
+  implicit def defaultBuilder[A: ClassTag] = new SpecializedArrayBuilder[A] {
+    val builder = ArrayBuilder.make[A]
+    def +=(a: A) = builder += a
+    def result = builder.result
+  }
+}
+
+object SpecializedArrayBuilder extends LowPrioritySpecializedArrayBuilder {
+  implicit val longBuilder = new SpecializedArrayBuilder[Long] {
+    val builder = new ArrayBuilder.ofLong
+    def +=(a: Long) = builder += a
+    def result = builder.result
+  }
+}
+
+final class Stream[@specialized(Long) T: ClassTag](val streamf: (T => Boolean) => Unit) {
+
+  def toArray()(implicit builder: SpecializedArrayBuilder[T]): Array[T] = {
+    foldLeft(builder)((b, v) => {b += v;b})
+    builder.result
+  }
 
   def filter(p: T => Boolean): Stream[T] =
     new Stream(iterf => streamf(value => !p(value) || iterf(value)))
 
-  def map[R: ClassTag](f: T => R): Stream[R] = 
+  def map[@specialized(Long) R: ClassTag](f: T => R): Stream[R] = 
     new Stream(iterf => streamf(value => iterf(f(value))))
 
   def takeWhile(p: T => Boolean): Stream[T] = 
@@ -56,14 +81,14 @@ final class Stream[T: ClassTag](val streamf: (T => Boolean) => Unit) {
     }))
   }
 
-  def flatMap[R: ClassTag](f: T => Stream[R]): Stream[R] = 
+  def flatMap[@specialized(Long) R: ClassTag](f: T => Stream[R]): Stream[R] = 
     new Stream(iterf => streamf(value => {
 	val innerf = f(value).streamf
 	innerf(iterf)
 	true
     }))
 
-  def foldLeft[A](a: A)(op: (A, T) => A): A = {
+  def foldLeft[@specialized(Long) A](a: A)(op: (A, T) => A): A = {
     var acc = a
     streamf(value => {
       acc = op(acc, value)
@@ -74,13 +99,13 @@ final class Stream[T: ClassTag](val streamf: (T => Boolean) => Unit) {
 
   def fold(z: T)(op: (T, T) => T): T = foldLeft(z)(op)
 
-  def size(): Long = foldLeft(0)((a: Int, _) => a + 1)
+  def size(): Long = foldLeft(0L)((a: Long, _) => a + 1L)
 
-  def sum[N >: T](implicit num: Numeric[N]): N = foldLeft(num.zero)(num.plus)
+  def sum[@specialized(Long) N >: T](implicit num: Numeric[N]): N = foldLeft(num.zero)(num.plus)
 }
 
 object Stream {
-  @inline def apply[T: ClassTag](xs: Array[T]) = {
+  @inline def apply[@specialized(Long) T: ClassTag](xs: Array[T]) = {
     val gen = (iterf: T => Boolean) => {
       var counter = 0
       var cont = true
